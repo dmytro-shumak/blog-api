66a5e854717d49469abe8c35d3bcda787bd4152f
Dmytro <dima06444@gmail.com>
2025-03-01 13:11:10 +0200

    feat: make server port configurable via environment variable

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+  const port = process.env.PORT || 3001;
 
-  await app.listen(3001);
+  await app.listen(port);
 }
 
 bootstrap();
